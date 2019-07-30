943256666ce4c6f3356613d6780d94a725452ab2
Singgit Bramantha <bambadom@gmail.com>
2019-07-30 21:33:08 +0700

    exercise8

---
+++ exercise7.js
@@ -0,0 +1,24 @@
+// SOAL NOMOR 1
+
+var rows1 = 5
+
+while(rows1 > 0 ) {
+    console.log('*');
+rows1 -= 1;
+}   
+
+console.log('------------------------------------------------------------')
+
+// SOAL NOMOR 2
+
+function tampilkanAngka(angkaPertama, angkaKedua) {
+    return angkaPertama + angkaKedua
+  }
+  
+  console.log(tampilkanAngka(9,1))
+
+
+
+
+
+
