55e060229a3a08e7e43c3ebf5240c988e3fd2365
Singgit Bramantha <bambadom@gmail.com>
2019-07-30 15:38:11 +0700

    exercise6

---
+++ exercise6.js
@@ -0,0 +1,34 @@
+// SOAL NOMOR 1
+
+console.log('LOOPING PERTAMA')
+
+var angka = 2;
+
+while(angka < 20 ) {
+    console.log(angka + ' - I love coding');
+angka += 2;
+}   
+
+console.log('LOOPING KEDUA')
+
+var angka2 = 20
+
+while(angka2 > 0 ) {
+    console.log(angka2 + ' - I will become full stack developer');
+angka2 -= 2;
+}   
+
+// SOAL NOMOR 2
+
+console.log('LOOPING PERTAMA')
+
+for(var angka3 = 1; angka3 < 20; angka3++) {
+    console.log(angka3 + ' - I love coding')}
+
+console.log('LOOPING KEDUA')
+
+for(var angka4 = 20; angka4 > 0; angka4--) {
+    console.log(angka4 + ' - I will become full stack developer')}
+
+    // SOAL NOMOR 3
+    
