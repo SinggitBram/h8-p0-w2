7f321d4dd1f41f767afdc3c44b89c7dc61c9119d
Singgit Bramantha <bambadom@gmail.com>
2019-07-31 16:01:56 +0700

    exercise12

---
+++ exercise12.js
@@ -0,0 +1,15 @@
+function konversiMenit(menit) {
+    var waktu = ''
+    var jam = ''
+    var menitan = ''
+
+jam = ((menit - (menit % 60)) / 60)
+menitan =  (menit % 60)  
+if (menitan < 10 ) {
+    menitan = '0' + menitan} 
+waktu = jam + ':' + menitan
+
+return waktu
+}
+  
+  console.log(konversiMenit(187)); // 1:03
