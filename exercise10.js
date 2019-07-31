eba151fe9a17864a763a14104c082295c9980f8c
Singgit Bramantha <bambadom@gmail.com>
2019-07-31 13:14:30 +0700

    exercise10

---
+++ exercise10.js
@@ -0,0 +1,15 @@
+
+function bandingkanAngka(angka1, angka2)
+{
+    if(angka2 > angka1) 
+    {
+    return ('true')}
+    else if (angka2 < angka1) 
+    {
+    return ('false')}
+    else 
+    {
+    return('-1')
+    }
+} 
+  console.log(bandingkanAngka(13, 9))
