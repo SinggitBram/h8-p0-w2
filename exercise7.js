90eb68c98b6eee2bf1166b7f2343f3ab234ef14b
Singgit Bramantha <bambadom@gmail.com>
2019-07-31 11:25:03 +0700

    exercise7perbaikan

--- exercise7.js
+++ exercise7.js
@@ -1,24 +1,42 @@
 // SOAL NOMOR 1
 
-var rows1 = 5
+var rows = 5
 
-while(rows1 > 0 ) {
-    console.log('*');
-rows1 -= 1;
-}   
+for (i=0; i<rows2; i++)
+{console.log('*')
+}
+   
 
 console.log('------------------------------------------------------------')
 
 // SOAL NOMOR 2
 
-function tampilkanAngka(angkaPertama, angkaKedua) {
-    return angkaPertama + angkaKedua
-  }
-  
-  console.log(tampilkanAngka(9,1))
+var rows2 = 5
+
+for (i=0; i<rows2; i++)
+{
+    var star = "*"
+    for (j=1; j<rows2; j++) 
+    {
+        star = star + "*"
+    }
+    console.log(star)
+}
 
 
 
+console.log('------------------------------------------------------------')
 
+// SOAL NOMOR 3
 
+var rows3 = 5
 
+for (i=0; i<rows3; i++)
+{
+    var star = "*"
+    for (j=0; j<i; j++) 
+    {
+        star = star + "*"
+    }
+    console.log(star)
+}
